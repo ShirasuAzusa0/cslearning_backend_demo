4280f67008babf56fef5b2152115e24b0c097f82
YZBen <lec16@foxmail.com>
2025-03-23 22:15:12 +0800

    修改neo4j连接配置

--- commons/contants.py
+++ commons/contants.py
@@ -5,10 +5,17 @@ LOGIN_SECRET = "f3raesfdkncf34209r3tj"
 MySQL_Config = 'mysql+mysqldb://root:Azusa1226!@127.0.0.1/postslist'
 
 # Neo4j配置
-# uri = "http://localhost:7474"
-uri = "http://120.76.138.103:7474/"
+'''
+uri = "http://localhost:7474"
 username = "neo4j"
 password ="AzusaNeo4j"
 auth = (username, password)
 name = "azusa"
+'''
+uri = "http://120.76.138.103:7474/"
+username = "neo4j"
+password ="AzusaNeo4j"
+auth = (username, password)
+name = "neo4j"
+
 Neo4j_Config = [uri, auth, name]
