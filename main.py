2ef4e4d1e6b781d1bbe6a557a1bd26d15d2a765f
YZBen <lec16@foxmail.com>
2025-03-25 21:07:59 +0800

    回退上一版本

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-#from resources import app
-from resources import create_app
-
-app = create_app()
+from resources import app
 
 # 后端程序运行入口（调试模式启动，端口为5000）
 if __name__ == "__main__":
--- resources/__init__.py
+++ resources/__init__.py
@@ -6,35 +6,24 @@ from py2neo import Graph
 
 from commons.contants import MySQL_Config, Neo4j_Config
 
-db = SQLAlchemy()
-api = Api()
+# 创建一个Flask应用实例app，在name为main时运行启动后端服务器
+app = Flask(__name__)
+# 创建一个Flask-RESTful API实例与Flask应用关联，设置了前端调用的后端接口的入口
+#api = Api(app)
+api = Api(app)
+# 配置SQLAlchemy数据库的链接信息，指定了链接的数据库
+app.config['SQLALCHEMY_DATABASE_URI'] = MySQL_Config
+# 创建一个SQLAlchemy实例并与Flask应用关联，以便使用映射方式获取数据库中的数据
+db = SQLAlchemy(app)
+# 配置Neo4j数据库的链接信息
+graph = Graph(Neo4j_Config[0], auth=Neo4j_Config[1], name=Neo4j_Config[2])
+# 将Neo4j驱动实例添加到Flask应用的配置中，以便在其他地方使用
+app.config['NEO4J_GRAPH'] = graph
 
-def create_app():
-    # 创建一个Flask应用实例app，在name为main时运行启动后端服务器
-    app = Flask(__name__)
-    # 创建一个Flask-RESTful API实例与Flask应用关联，设置了前端调用的后端接口的入口
-    #api = Api(app)
-    api.init_app(app)
-    # 配置SQLAlchemy数据库的链接信息，指定了链接的数据库
-    app.config['SQLALCHEMY_DATABASE_URI'] = MySQL_Config
-    # 创建一个SQLAlchemy实例并与Flask应用关联，以便使用映射方式获取数据库中的数据
-    #db = SQLAlchemy(app)
-    db.init_app(app)
-    # 配置Neo4j数据库的链接信息
-    graph = Graph(Neo4j_Config[0], auth=Neo4j_Config[1], name=Neo4j_Config[2])
-    # 将Neo4j驱动实例添加到Flask应用的配置中，以便在其他地方使用
-    app.config['NEO4J_GRAPH'] = graph
-
-    from resources import learningroute_resource
-    from resources import attachment_resource
-    from resources import posts_resource
-    from resources import conversations_resource
-    from resources import users_resource
-    from resources import neo4j_resource
-    from resources import categories_resource
-
-    return app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run()
+from resources import learningroute_resource
+from resources import attachment_resource
+from resources import posts_resource
+from resources import conversations_resource
+from resources import users_resource
+from resources import neo4j_resource
+from resources import categories_resource
